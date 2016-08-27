78372f53702d745fdb57b5389f6ffa7aaffb4cf2
Albert Nguyen <Albert.la.nguyen@gmail.com>
2016-08-27 12:53:57 -0700

    added users and session routes

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,37 @@
 class Api::UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login(@user)
+      render "api/users/show"
+    else
+      @errors = @user.errors.full_messages
+      render "api/shared/error", status: 422
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      render :show
+    else
+      @errors = @user.errors.full_messages
+      render "api/shared/error", status: 422
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :email)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root to: "static_pages#root"
 
     namespace :api, defaults: {format: :json} do
+      resources :users, only: [:create, :show, :edit, :update]
+      resource :session, only: [:create, :destroy, :show]
       resources :images, only: [:show, :index, :create]
     end
 end
